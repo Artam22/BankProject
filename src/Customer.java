221a9e22e1ab6ab008c99b66c327668305f9d563
Artam <erdemmmgurel@gmail.com>
2024-02-07 11:29:22 +0300

    Customer Done V1

--- src/Customer.java
+++ src/Customer.java
@@ -9,7 +9,12 @@ public class Customer {
      customerId Integer,
      firstName, lastName, city String OLACAK
      */
+    private final int customerId;
+    private String firstName;
+    private String lastName;
+    private String city;
 
+    private static int idC = 1;
 
 
     ///////////////////////////////////////////////////////////////////////////////////
@@ -18,26 +23,60 @@ public class Customer {
     // TODO BOŞ CONSTRUCTOR OLUŞTURUN
     // PARAMTRELERİ int customerId, String firstName, String lastName, String city OLAN CONSTRUCTOR OLUŞTURUN
 
-
-
-
+    public Customer(String firstName, String lastName, String city) {
+        this.customerId = idC++;
+        setFirstName(firstName);
+        setLastName(lastName);
+        setCity(city);
+    }
 
     ////////////////////////////////////////////////////////////////////////////////////
 
     // GETTER SETTER METHODS
     // TODO  GETTER VE SETTER METODLARI OLUŞTURUN
 
+    public int getCustomerId() {
+        return customerId;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 
 
     ////////////////////////////////////////////////////////////////////////////////
 
     //  TODO TO STRING METODU OLUŞTURUN
 
-
-
-
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
 
 
     /////////////////////////////////////////////////////////////////////////////////
