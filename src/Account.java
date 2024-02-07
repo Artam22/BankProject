4082e5a78009d310a86fa28da02eba57b44da11c
Artam <erdemmmgurel@gmail.com>
2024-02-07 11:39:27 +0300

    Account Done V2

--- src/Account.java
+++ src/Account.java
@@ -3,27 +3,52 @@
 public class Account {
 
     // FIELDS
-    private int accountId;
+    private final int accountId;
     private int customerId;
     private double balance;
     private AccountType accountType;
 
-    // CONSTRUCTORS
-    public Account() {
-    }
+    private static int idC = 1000;
 
-    public Account(int accountId, int customerId, AccountType accountType) {
-        this.accountId = accountId;
-        this.customerId = customerId;
-        this.accountType = accountType;
+
+    // CONSTRUCTORS
+
+    public Account(int customerId, AccountType accountType) {
+        this.accountId = idC++;
+        setCustomerId(customerId);
+        setAccountType(accountType);
     }
 
     // GETTER AND SETTER METHODS
     // TODO GETTER VE SETTER METODLARINI OLUŞTURUN
 
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
 
+    public int getAccountId() {
+        return accountId;
+    }
 
+    public int getCustomerId() {
+        return customerId;
+    }
 
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
 
 
     ////////////////////////////////////////////////////////////////////////////
@@ -39,8 +64,13 @@ public class Account {
       3. BÜYÜKSE BAKİYEYE İLAVE EDİP KULLANICIYA MESAJ VERİN
       4. YATIRILMAK İSTENEN TUTAR SIFIRDAN KÜÇÜKSE KULLANICIYA MESAJ VERİN, YENİDEN DENEMESİNİ İSTEYİN...
      */
-
-
+        if (amount > 0){
+            this.balance += amount;
+            System.out.println("New balance= " + this.balance);
+        }
+        else {
+            System.out.println("Please enter a valid number.");
+        }
 
 
 
@@ -52,7 +82,6 @@ public class Account {
     //  ÇEKİLECEK TUTARIN SIFIRDAN BÜYÜK OLMASI GEREKİR.
     //  ŞART SAĞLANDIĞINDA İLGİLİ HESABIN BAKİYESİ TUTAR KADAR AZALMIŞ OLMALIDIR.
     public void withdrawToAmount(double amount) {
-
         /*
          1. BU METODU BankSimulation SINIFINDAKİ withdrawToAccount() METODUNDA DİREKT ÇAĞIRARAK KULLANIN
          2. ÇEKİLMEK İSTENEN TUTAR, NEGATİF Mİ, DEĞİL Mİ KONTROL EDİN
@@ -60,19 +89,28 @@ public class Account {
          4. ÇEKİLMEK İSTENEN TUTAR (amount) POZİTİFSE VE BAKİYEDEN KÜÇÜKSE,BAKİYEDEN DÜŞÜP,YENİ BAKİYEYİ KULLANICIYA BİLDİRİN...
          5. YATIRILMAK İSTENEN TUTAR SIFIRDAN KÜÇÜKSE YA DA BAKİYEDEN BÜYÜKSE,KULLANICIYA MESAJ VERİN, YENİDEN DENEMESİNİ İSTEYİN
          */
-
-
-
-
+        if (amount > 0 && amount < this.balance){
+            this.balance -= amount;
+            System.out.println("New balance= " + this.balance);
+        }
+        else {
+            System.out.println("Please enter a valid number.");
+        }
 
     }
     /////////////////////////////////////////////////////////////////////////////////////
 
     // TODO TO STRING METODUNU OLUŞTURUN
 
-
-
-
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountId=" + accountId +
+                ", customerId=" + customerId +
+                ", balance=" + balance +
+                ", accountType=" + accountType +
+                '}';
+    }
 
 
     ////////////////////////////////////////////////////////////////////////////////////////
